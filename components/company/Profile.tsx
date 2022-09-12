81eae0de86ef5c6d5fd1fd3f903daa29cf53b72c
Julian Pagtama <jpagtama@gmail.com>
2022-09-12 11:06:10 -0700

    Build: Add company details components

--- components/company/Profile.tsx
+++ components/company/Profile.tsx
@@ -1,45 +1,195 @@
-import React from 'react'
+import {useState} from 'react'
+import {CompanyOverview, CustomError, EarningsData, Status, StockData, StockPrice} from '../../types'
+import {Chart as ChartJS, CategoryScale, LinearScale, BarElement, PointElement, LineElement, Title, Tooltip, Legend } from 'chart.js'
+import type {ChartOptions} from 'chart.js'
+import {Line, Bar} from 'react-chartjs-2'
 import styles from '../../styles/company/Profile.module.css'
 
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+);
+
 interface Props {
-  movingAverage50day: string
-  high52week: string
-  low52week: string
-  movingAverage200day: string
-  Address: string
-  AnalystTargetPrice: string
-  AssetType: string
-  CIK: string
-  Country: string
-  Description: string
-  Exchange: string
-  FiscalYearEnd: string
-  ForwardPE: string
-  Industry: string
-  LatestQuarter: string
-  MarketCapitalization: string
-  Name: string
-  SharesOutstanding: string
-  Symbol: string
+  details: CompanyOverview | CustomError
+  daily: StockData | CustomError
+  earnings: EarningsData | CustomError
+  status: Status
 }
 
-const Profile = (props: Props) => {
+const Profile = ({details, daily, earnings, status}: Props) => {
+  const [graphMode, setGraphMode] = useState('1m')
+
+  const symbol = `${details && 'Symbol' in details? details.Symbol : ''}`
+  const name = `${details && 'Name' in details? details.Name : ''}`
+  const desc = `${details && 'Description' in details? details.Description : ''}`
+  const exch = `${details && 'Exchange' in details? details.Exchange : ''}`
+  const mcap = `${details && 'MarketCapitalization' in details? details.MarketCapitalization : ''}`
+  const target = `${details && 'AnalystTargetPrice' in details? details.AnalystTargetPrice : ''}`
+  const sharesOutstanding = `${details && 'SharesOutstanding' in details? details.SharesOutstanding : ''}`
+  const forPE = `${details && 'ForwardPE' in details? details.ForwardPE : ''}`
+  const ma50 = `${details && 'movingAverage50day' in details? details.movingAverage50day : ''}`
+  const ma200 = `${details && 'movingAverage200day' in details? details.movingAverage200day : ''}`
+  const fiscYrEnd = `${details && 'FiscalYearEnd' in details? details.FiscalYearEnd : ''}`
+
+  const optionsLine = {
+    responsive: true,
+    plugins: {
+        legend: {
+            display: false
+        }
+    }
+  }
+
+  const optionsBar: ChartOptions = {
+    responsive: true,
+    plugins: {
+        legend: {
+            reverse: true,
+            position: 'top'
+        },
+        title: {
+            display: false,
+            text: ''
+        }
+    }
+  }
+
+  let labels: string[] = []
+  let prices: number[] = []
+
+  if (daily && 'labels' in daily) {
+    switch(graphMode) {
+        case '7d':
+            labels = daily.labels.slice(daily.labels.length - 7)
+            prices = daily.price.slice(daily.labels.length - 7).map(item => item.close)
+            break
+        case '2wk':
+            labels = daily.labels.slice(daily.labels.length - 14)
+            prices = daily.price.slice(daily.labels.length - 14).map(item => item.close)
+            break
+        case '1m':
+            labels = daily.labels.slice(daily.labels.length - 30)
+            prices = daily.price.slice(daily.labels.length - 30).map(item => item.close)
+            break
+        case '3m':
+            labels = daily.labels.slice(daily.labels.length - 90)
+            prices = daily.price.slice(daily.labels.length - 90).map(item => item.close)
+            break
+        default:
+            labels = daily.labels.slice(daily.labels.length - 7)
+            prices = daily.price.slice(daily.labels.length - 7).map(item => item.close)
+    }
+  }
+
+  const dataStock = {
+    labels: labels,
+    datasets: [{
+        label: symbol,
+        data: prices,
+        borderColor: '#f900bf',
+        backgroundColor: '#f900bf',
+    }]
+  }
+
+  const companyOverview = () => {
+    return (
+      <>
+        <h2 className={styles.descLabel}>Description:</h2>
+        <p className={styles.desc}>{desc}</p>
+
+        <h2 className={styles.descLabel}>Stats:</h2>
+        <div className={styles.overviewContainer}>
+            <div className={styles.overviewSection}>
+                <span>Ticker Symbol:</span> <span>{symbol}</span>
+            </div>
+            <div className={styles.overviewSection}>
+                <span>Exchange:</span> <span>{exch}</span>
+            </div>
+            <div className={styles.overviewSection}>
+                <span>Market Cap:</span> <span>{mcap}</span>
+            </div>
+            <div className={styles.overviewSection}>
+                <span>Analyst Target Price:</span> <span>${target}</span>
+            </div>        
+            <div className={styles.overviewSection}>
+                <span>Shares Outstanding:</span> <span>{sharesOutstanding}</span>
+            </div>
+            <div className={styles.overviewSection}>
+                <span>Forward PE:</span> <span>{forPE}</span>
+            </div>   
+            <div className={styles.overviewSection}>
+                <span>50 Day Moving Avg:</span> <span>${ma50}</span>
+            </div>        
+            <div className={styles.overviewSection}>
+                <span>200 Day Moving Avg:</span> <span>${ma200}</span>
+            </div>        
+            <div className={styles.overviewSection}>
+                <span>Fiscal Year End:</span> <span>{fiscYrEnd}</span>
+            </div>        
+        </div>
+        <div className={styles.earningsContainer}>
+            
+        </div>
+      </>
+    )
+  }
+
+//   const earningsOverview = (labels: string[], reportedEps: number[], estimatedEps: number[]) => {
+  const earningsOverview = () => {
+    if ('labels' in earnings) {
+        const labels = earnings.labels.slice(0,4).reverse()
+        const reportedEps = earnings.reportedEPS.slice(0,4).reverse()
+        const estEps = earnings.estimatedEPS.slice(0,4).reverse()
+
+        const data = {
+            labels: labels,
+            datasets: [{
+                label: 'Reported EPS',
+                data: reportedEps,
+                borderColor: '#F900BF',
+                backgroundColor: '#F900BF',
+                },
+                {
+                label: 'Estimated EPS',
+                data: estEps,
+                borderColor: '#293462',
+                backgroundColor: '#293462',
+                }
+            ]
+        }
+        return (
+            <Bar options={optionsBar} data={data} />
+        )
+    }
+    
+  }
+
+  const displayError = (message: string) => {
+    return (
+        <div className={styles.errContainer}>
+            <p className={styles.errMessage}>Well, this is awkward. This free API has encountered some limitations:</p>
+            <p className={styles.errMessage}>"{message}"</p>
+        </div>
+    )
+  }
+
   return (
-    <div>
-      <span className={styles.symbol}>{props.Symbol} </span>
-      <h1 className={styles.companyName} >{props.Name}</h1>
-      <p><span>Exchange:</span> {props.Exchange}</p>
-      <p><span>Industry:</span> {props.Industry}</p>
-      <p><span>Address:</span> {props.Address}</p>
-      <p><span>Country:</span> {props.Country}</p>
-      <p><span>Market Cap:</span> {props.MarketCapitalization}</p>
-      <p><span>50 Day Moving Avg:</span> ${props.movingAverage50day}</p>
-      <p><span>200 Day Moving Avg:</span> ${props.movingAverage200day}</p>
-      <p><span>Analyst Target Price:</span> ${props.AnalystTargetPrice}</p>
-      <p><span>Fiscal Year End:</span> {props.FiscalYearEnd}</p>
-      <p><span>Forward PE:</span> {props.ForwardPE}</p>
-      <p><span>Shares Outstanding:</span> {props.SharesOutstanding}</p>
-      <p><span>Description:</span> {props.Description}</p>
+    <div className={styles.companyProfileContainer}>
+
+      <h1 className={styles.companyName} >{name}</h1>
+      
+      {!('error_message' in daily)? <div className={styles.chartContainer}><Line options={optionsLine} data={dataStock} /></div> : displayError(daily.error_message)}
+      {!('error_message' in details)? companyOverview() : displayError(details.error_message)}
+      {/* {!('error_message' in earnings)? earningsOverview(earnings.labels, earnings.estimatedEPS, earnings.reportedEPS): displayError(earnings.error_message)} */}
+      {!('error_message' in earnings)? earningsOverview(): displayError(earnings.error_message)}
+
     </div>
   )
 }
--- pages/company/[ticker].tsx
+++ pages/company/[ticker].tsx
@@ -1,42 +1,22 @@
 import { GetStaticPropsContext } from "next"
+import {CompanyOverview, StockPrice, StockData, Status, CustomError, EarningsData } from '../../types'
 import { isJSONEmpty } from "../../utils/utils"
 import Profile from '../../components/company/Profile'
 
 interface Props {
-  details: {
-    movingAverage50day: string
-    high52week: string
-    low52week: string
-    movingAverage200day: string
-    Address: string
-    AnalystTargetPrice: string
-    AssetType: string
-    CIK: string
-    Country: string
-    Description: string
-    Exchange: string
-    FiscalYearEnd: string
-    ForwardPE: string
-    Industry: string
-    LatestQuarter: string
-    MarketCapitalization: string
-    Name: string
-    SharesOutstanding: string
-    Symbol: string 
-    message?: string
-  }
-  status: string
+  details: CompanyOverview | CustomError
+  daily: StockData | CustomError
+  earnings: EarningsData | CustomError
+  status: Status
 }
 
 const Details = (props: Props) => {
 
-  // console.log('props :>> ', props);
-  const companyProfile = props.status === 'ok'? <Profile {...props.details} />: props.status
+  const companyProfile = <Profile {...props} />
 
   return (
     <>
       {companyProfile}
-
     </>
   )
 }
@@ -45,41 +25,15 @@ export const getStaticPaths = async () => {
   return {
     fallback: true,
     paths: [
-      {
-        params: {
-          ticker: 'AAPL'
-        }
-      },
-      {
-        params: {
-          ticker: 'TSLA'
-        }
-      },
-      {
-        params: {
-          ticker: 'AMZN'
-        }
-      },
-      {
-        params: {
-          ticker: 'MSFT'
-        }
-      },
-      {
-        params: {
-          ticker: 'GOOG'
-        }
-      },
-      {
-        params: {
-          ticker: 'GOOGL'
-        }
-      },
-      {
-        params: {
-          ticker: 'JNJ'
-        }
-      },
+      {params: {ticker: 'AAPL'}},
+      {params: {ticker: 'TSLA'}},
+      {params: {ticker: 'AMZN'}},
+      {params: {ticker: 'MSFT'}},
+      {params: {ticker: 'GOOG'}},
+      {params: {ticker: 'GOOGL'}},
+      {params: {ticker: 'JNJ'}},
+      {params: {ticker: 'GME'}},
+      {params: {ticker: 'BBBY'}},
     ]
   }
 }
@@ -87,25 +41,84 @@ export const getStaticPaths = async () => {
 export const getStaticProps = async(context: GetStaticPropsContext) => {
 
   const ticker = context.params?.ticker?.toString().trim()
-  let details: Record<string, any> = {}
-  let status = 'ok'
+  let details: CompanyOverview | CustomError | undefined
+  let daily: StockData | CustomError | undefined
+  let earnings: EarningsData | CustomError | undefined
+  let status: Status = {status: "ok"}
 
   if (ticker) {
     try {
-      const response = await fetch(`https://www.alphavantage.co/query?function=OVERVIEW&symbol=${ticker}&apikey=${process.env.ALPHAVANTAGE_APIKEY}`)
-      const data = await response.json()
-      if (isJSONEmpty(data)) throw new Error('Unable to find company')
-      details = {
-        ...data, 
-        movingAverage50day: data['50DayMovingAverage'], 
-        movingAverage200day: data['200DayMovingAverage'],
-        high52week: data['52WeekHigh'],
-        low52week: data['52WeekLow']
+      const resOverview = await fetch(`https://www.alphavantage.co/query?function=OVERVIEW&symbol=${ticker}&apikey=${process.env.ALPHAVANTAGE_APIKEY}`)
+      const dataOverview = await resOverview.json()
+      // console.log('dataOverview :>> ', dataOverview);
+
+      const resDaily = await fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${ticker}&apikey=${process.env.ALPHAVANTAGE_APIKEY}`)
+      const dataDaily = await resDaily.json()
+      // console.log('dataDaily :>> ', dataDaily);
+
+      const resEPS = await fetch(`https://www.alphavantage.co/query?function=EARNINGS&symbol=${ticker}&apikey=${process.env.ALPHAVANTAGE_APIKEY}`)
+      const dataEPS = await resEPS.json()
+
+      // const res5min = await fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${ticker}&interval=5min&apikey=${process.env.ALPHAVANTAGE_APIKEY}`)
+      // const data5min = await res5min.json()
+      // console.log('data5min', data5min)
+
+      if (isJSONEmpty(dataOverview)) {
+        details = {error_message: 'Company overview not available'}
+      } else if (dataOverview.Note) {
+        details = {error_message: dataOverview.Note}
+      } else {
+        details = {
+          ...dataOverview,
+          movingAverage50day: dataOverview['50DayMovingAverage'],
+          movingAverage200day: dataOverview['200DayMovingAverage'],
+          high52week: dataOverview['52WeekHigh'],
+          low52week: dataOverview['52WeekLow']
+        }
       }
+
+      const dailyData = {...dataDaily['Time Series (Daily)']}
+
+      if (isJSONEmpty(dailyData)) {
+        daily = {error_message: 'Stock data not available'}
+      } else if (dailyData.Note) {
+        daily = {error_message: 'Stock data not available at this time'}
+      } else {
+        const labels = Object.keys(dailyData).sort()
+        const price = labels.map(item => ({
+          open: parseFloat(dailyData[item]['1. open']),
+          high: parseFloat(dailyData[item]['2. high']),
+          low: parseFloat(dailyData[item]['3. low']),
+          close: parseFloat(dailyData[item]['4. close']),
+          volume: parseFloat(dailyData[item]['5. volume'])
+        }))
+        daily = {labels, price}
+      }
+
+      const epsData = dataEPS.quarterlyEarnings
+
+      if (epsData.length === 0) {
+        earnings = {error_message: 'Earnings data not available'}
+      } else if (epsData.Note) {
+        earnings = {error_message: 'Earnings data not available at this time'}
+      } else {
+        const labels: string[] = []
+        const reportedEPS: number[] = [] 
+        const estimatedEPS: number[] = []
+
+        for (const i of epsData) {
+          labels.push(i.reportedDate)
+          reportedEPS.push(parseFloat(i.reportedEPS))
+          estimatedEPS.push(parseFloat(i.estimatedEPS))
+        }
+
+        earnings = {labels, reportedEPS, estimatedEPS}
+      }
+
     } catch (err) {
       if (err instanceof Error) {
-        details.message = err.message
-        status = 'error'
+        status.message = err.message
+        status.status = 'error'
       }
     }
   }
@@ -113,8 +126,10 @@ export const getStaticProps = async(context: GetStaticPropsContext) => {
   return {
     props: {
       details,
+      daily,
+      earnings,
       status
-    }
+    },
   }
 }
 
