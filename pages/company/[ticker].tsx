3d984a2dc17bdf5502525bf3be0e2b93f28816ab
Julian Pagtama <jpagtama@gmail.com>
2022-09-13 17:56:19 -0700

    Feat: Add api call for earnings and format data

--- pages/company/[ticker].tsx
+++ pages/company/[ticker].tsx
@@ -1,5 +1,5 @@
 import { GetStaticPropsContext } from "next"
-import {CompanyOverview, StockPrice, StockData, Status, CustomError, EarningsData } from '../../types'
+import {CompanyOverview, StockPrice, StockData, Status, CustomError, EarningsData, EarningsCalendar } from '../../types'
 import { isJSONEmpty } from "../../utils/utils"
 import Profile from '../../components/company/Profile'
 
@@ -7,6 +7,7 @@ interface Props {
   details: CompanyOverview | CustomError
   daily: StockData | CustomError
   earnings: EarningsData | CustomError
+  earnings_calendar: EarningsCalendar[] | CustomError
   status: Status
 }
 
@@ -41,23 +42,25 @@ export const getStaticPaths = async () => {
 export const getStaticProps = async(context: GetStaticPropsContext) => {
 
   const ticker = context.params?.ticker?.toString().trim()
-  let details: CompanyOverview | CustomError | undefined
-  let daily: StockData | CustomError | undefined
-  let earnings: EarningsData | CustomError | undefined
+  let details: CompanyOverview | CustomError | {} = {}
+  let daily: StockData | CustomError | {} = {}
+  let earnings: EarningsData | CustomError | {} = {}
+  let earnings_calendar: EarningsCalendar[] | CustomError | [] = []
   let status: Status = {status: "ok"}
 
   if (ticker) {
     try {
       const resOverview = await fetch(`https://www.alphavantage.co/query?function=OVERVIEW&symbol=${ticker}&apikey=${process.env.ALPHAVANTAGE_APIKEY}`)
       const dataOverview = await resOverview.json()
-      // console.log('dataOverview :>> ', dataOverview);
 
       const resDaily = await fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${ticker}&apikey=${process.env.ALPHAVANTAGE_APIKEY}`)
       const dataDaily = await resDaily.json()
-      // console.log('dataDaily :>> ', dataDaily);
 
       const resEPS = await fetch(`https://www.alphavantage.co/query?function=EARNINGS&symbol=${ticker}&apikey=${process.env.ALPHAVANTAGE_APIKEY}`)
       const dataEPS = await resEPS.json()
+
+      const resEarningsCal = await fetch(`https://www.alphavantage.co/query?function=EARNINGS_CALENDAR&symbol=${ticker}&horizon=12month&apikey=${process.env.ALPHAVANTAGE_APIKEY}`)
+      const dataEarningsCal = await resEarningsCal.text()
 
       // const res5min = await fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${ticker}&interval=5min&apikey=${process.env.ALPHAVANTAGE_APIKEY}`)
       // const data5min = await res5min.json()
@@ -97,9 +100,9 @@ export const getStaticProps = async(context: GetStaticPropsContext) => {
 
       const epsData = dataEPS.quarterlyEarnings
 
-      if (epsData.length === 0) {
+      if (epsData === 'undefined' || epsData.length === 0) {
         earnings = {error_message: 'Earnings data not available'}
-      } else if (epsData.Note) {
+      } else if (dataEPS.Note) {
         earnings = {error_message: 'Earnings data not available at this time'}
       } else {
         const labels: string[] = []
@@ -115,8 +118,34 @@ export const getStaticProps = async(context: GetStaticPropsContext) => {
         earnings = {labels, reportedEPS, estimatedEPS}
       }
 
+      if (dataEarningsCal === 'undefined' || typeof dataEarningsCal !== 'string' || dataEarningsCal.length === 0) {
+        earnings_calendar = {error_message: 'Earnings calendar data not available'}
+      } else {
+        const earningsCalDataFields = dataEarningsCal.split(/[\n|,]/).slice(0,6)
+        const earningsCalData = dataEarningsCal.split(/[\n|,]/).slice(6)
+        let earningsCalendar = []
+        // console.log('earningsCalDataFields :>> ', earningsCalDataFields);
+        // console.log('ungabunga :>> ', earningsCalData);
+
+        // Convert csv data to json
+        for (let i = 0; i < earningsCalData.length; i++) {
+          let item: {[key: string]: string} = {} 
+          for (let j = 0; j < earningsCalDataFields.length; j++) {
+            let field = earningsCalDataFields[j]? earningsCalDataFields[j].replace(/(\r\n|\n|\r)/gm, "") : ''
+            let fieldValue = earningsCalData[i]? earningsCalData[i].replace(/(\r\n|\n|\r)/gm, "") : ''
+            item[field] = fieldValue
+            if (j !== earningsCalDataFields.length - 1) i = i + 1
+          }
+          earningsCalendar.push(item)
+        }
+        // console.log('earningsCalendar BEFORE', earningsCalendar)
+        earnings_calendar = earningsCalendar.length > 0? earningsCalendar: {error_message: 'Earnings calendar data not available'}
+        // console.log('earnings_calendar AFTER :>> ', earnings_calendar);
+      }
+      
     } catch (err) {
       if (err instanceof Error) {
+        console.log("Reached Error:", err.message)
         status.message = err.message
         status.status = 'error'
       }
@@ -128,6 +157,7 @@ export const getStaticProps = async(context: GetStaticPropsContext) => {
       details,
       daily,
       earnings,
+      earnings_calendar,
       status
     },
   }
