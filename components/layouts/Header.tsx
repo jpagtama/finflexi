7824ba8a10334717571f612efbca888713ac0aea
Julian Pagtama <jpagtama@gmail.com>
2022-09-20 11:54:47 -0700

    Feat: Add menu behavior when menu is opened or closed

--- components/layouts/Header.tsx
+++ components/layouts/Header.tsx
@@ -1,11 +1,19 @@
-import Menu from '../UI/Menu'
-import styles from '../../styles/layouts/Header.module.css'
+import Menu from '@components/UI/Menu'
+import MenuList from '@components/UI/MenuList'
+import { useSelector } from 'react-redux'
+import styles from '@styles/layouts/Header.module.css'
+
 
 const Header = () => {
+  const isMenuOpen = useSelector((state: {"menu":{"open": boolean}}) => state.menu.open)
+
   return (
-    <header className={styles.header}>
+    <>
+    <header className={`${styles.header} ${isMenuOpen && styles.menuIsOpen}`}>
       <Menu />
+      {isMenuOpen && <MenuList />}
     </header>
+    </>
   )
 }
 
