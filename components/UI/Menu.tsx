405a24f95e1d996270c0adb62ebadef9d7cd1ca4
Julian Pagtama <jpagtama@gmail.com>
2022-09-20 11:53:38 -0700

    Feat: Add connection between menu and redux store

--- components/UI/Menu.tsx
+++ components/UI/Menu.tsx
@@ -1,16 +1,18 @@
-import {useState} from 'react'
-import styles from '../../styles/UI/Menu.module.css'
+import { menuActions } from 'store'
+import { useSelector, useDispatch } from 'react-redux'
+import styles from '@styles/UI/Menu.module.css'
 
 const Menu = () => {
-    const [open, setOpen] = useState(false)
+    const dispatch = useDispatch()
+    const menuState = useSelector((state: {"menu": {"open": boolean}}) => state.menu)
 
     const menuHandler = () => {
-        setOpen(!open)
+        dispatch(menuActions.toggle())
     }
 
     return (
         <button className={styles.menu} onClick={menuHandler}>
-            <div className={`${open && styles.isActive} ${styles.hamburger}`} id="burgerMenu">
+            <div className={`${menuState.open && styles.isActive} ${styles.hamburger}`} id="burgerMenu">
                 <span className={styles.line}></span>
                 <span className={styles.line}></span>
                 <span className={styles.line}></span>
