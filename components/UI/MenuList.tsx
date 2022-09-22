f25dbd57e164d3e5a8311f3981122aaed59cf3d3
Julian Pagtama <jpagtama@gmail.com>
2022-09-21 18:46:02 -0700

    Feat: modify menu list items to close menu on click

--- components/UI/MenuList.tsx
+++ components/UI/MenuList.tsx
@@ -1,5 +1,5 @@
 import { menuActions } from '@store/index'
-import {useDispatch, useSelector} from 'react-redux'
+import {useDispatch} from 'react-redux'
 import { useRouter } from 'next/router'
 import styles from '@styles/UI/MenuList.module.css'
 import React from 'react'
@@ -16,8 +16,9 @@ const MenuList = () => {
   return (
     <div className={styles.container}>
         <ul className={styles.menuList}>
-            <li><span onClick={(e) => clickHandler(`${process.env.NEXT_PUBLIC_BASE_URL}/company/search/`)} >Search</span></li>
-            <li><span onClick={(e) => clickHandler(`${process.env.NEXT_PUBLIC_BASE_URL}/economy/`)} >Economy</span></li>
+            <li><span onClick={() => clickHandler("/")} >Home</span></li>
+            <li><span onClick={() => clickHandler("/company/search")} >Search</span></li>
+            <li><span onClick={() => clickHandler("/economy")} >Economy</span></li>
             <li>Filings</li>
         </ul>
     </div>
