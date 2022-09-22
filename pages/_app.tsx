51be4f0b7710cab183da7d162753faeee7ed2acd
Julian Pagtama <jpagtama@gmail.com>
2022-09-21 18:44:32 -0700

    Feat: add loading screen using router from next/router

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,42 @@
+import {useState, useEffect} from 'react'
 import type { AppProps } from 'next/app'
-import Main from '../components/layouts/Main'
 import {Provider} from 'react-redux'
 import store from 'store'
-import '../styles/globals.css'
+import { useRouter } from 'next/router'
+import Main from '@components/layouts/Main'
+import Loading from '@components/UI/Loading'
+import '@styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+
+  //   console.log('router.asPath :>> ', router.asPath);
+  //   console.log('is loading? :>> ', loading);
+    // if (router.asPath !== '/') {
+      const handleStart = (url: string) => setLoading(true) 
+      const handleComplete = (url: string) => setLoading(false)
+
+      router.events.on('routeChangeStart', handleStart)
+      // console.log('route is changing')
+      router.events.on('routeChangeComplete', handleComplete)
+      router.events.on('routeChangeError', handleComplete)
+      // console.log('route is complete')
+
+      return () => {
+        router.events.off('routeChangeStart', handleStart)
+        router.events.off('routeChangeComplete', handleComplete)
+        router.events.off('routeChangeError', handleComplete)
+      }
+    // }
+    // console.log('loading? :>>', loading)
+  }, [router])
+
   return (
     <Provider store={store} >
+      {loading && <Loading />}
       <Main >
         <Component {...pageProps} />
       </Main>
