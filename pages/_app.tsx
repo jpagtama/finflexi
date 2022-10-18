c255440bd209f53157f3cb0b3bab64e44947bdaa
Julian Pagtama <jpagtama@gmail.com>
2022-10-18 10:35:52 -0700

    Format for readability

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
 import type { AppProps } from 'next/app'
-import {Provider} from 'react-redux'
+import { Provider } from 'react-redux'
 import store from 'store'
 import { useRouter } from 'next/router'
 import Main from '@components/layouts/Main'
@@ -11,25 +11,26 @@ function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter()
   const [loading, setLoading] = useState(false)
 
+
   useEffect(() => {
 
-  //   console.log('router.asPath :>> ', router.asPath);
-  //   console.log('is loading? :>> ', loading);
+    //   console.log('router.asPath :>> ', router.asPath);
+    //   console.log('is loading? :>> ', loading);
     // if (router.asPath !== '/') {
-      const handleStart = (url: string) => setLoading(true) 
-      const handleComplete = (url: string) => setLoading(false)
+    const handleStart = (url: string) => setLoading(true)
+    const handleComplete = (url: string) => setLoading(false)
 
-      router.events.on('routeChangeStart', handleStart)
-      // console.log('route is changing')
-      router.events.on('routeChangeComplete', handleComplete)
-      router.events.on('routeChangeError', handleComplete)
-      // console.log('route is complete')
+    router.events.on('routeChangeStart', handleStart)
+    // console.log('route is changing')
+    router.events.on('routeChangeComplete', handleComplete)
+    router.events.on('routeChangeError', handleComplete)
+    // console.log('route is complete')
 
-      return () => {
-        router.events.off('routeChangeStart', handleStart)
-        router.events.off('routeChangeComplete', handleComplete)
-        router.events.off('routeChangeError', handleComplete)
-      }
+    return () => {
+      router.events.off('routeChangeStart', handleStart)
+      router.events.off('routeChangeComplete', handleComplete)
+      router.events.off('routeChangeError', handleComplete)
+    }
     // }
     // console.log('loading? :>>', loading)
   }, [router])
