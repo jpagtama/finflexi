c0c67e928483db50a6c9a50af04dd9748fdcbc8e
Julian Pagtama <jpagtama@gmail.com>
2022-10-19 09:54:46 -0700

    Add helper to convert DB date value to string

--- utils/utils.tsx
+++ utils/utils.tsx
@@ -2,3 +2,12 @@
 export const isJSONEmpty = (obj: Object) => {
     return JSON.stringify(obj) === '{}'
 }
+
+// date argument must come from DB column of type date
+export const dbDatetoString = (date: Date) => {
+    let d = null
+    d = new Date(date)
+    d.setDate(d.getDate() + 1)
+    d = d.toLocaleDateString('en-US')
+    return d
+}
