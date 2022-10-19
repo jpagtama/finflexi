5b1d4ba60d3e956c011ea89c72c34486c308d584
Julian Pagtama <jpagtama@gmail.com>
2022-10-19 09:48:14 -0700

    Add signup to menu list

--- components/UI/MenuList.tsx
+++ components/UI/MenuList.tsx
@@ -1,5 +1,5 @@
 import { menuActions } from '@store/index'
-import {useDispatch} from 'react-redux'
+import { useDispatch } from 'react-redux'
 import { useRouter } from 'next/router'
 import styles from '@styles/UI/MenuList.module.css'
 import React from 'react'
@@ -15,12 +15,12 @@ const MenuList = () => {
 
   return (
     <div className={styles.container}>
-        <ul className={styles.menuList}>
-            <li><span onClick={() => clickHandler("/")} >Home</span></li>
-            <li><span onClick={() => clickHandler("/company/search")} >Search</span></li>
-            <li><span onClick={() => clickHandler("/economy")} >Economy</span></li>
-            <li>Filings</li>
-        </ul>
+      <ul className={styles.menuList}>
+        <li><span onClick={() => clickHandler("/")} >Home</span></li>
+        <li><span onClick={() => clickHandler("/signup")} >Sign-Up</span></li>
+        <li><span onClick={() => clickHandler("/company/search")} >Search</span></li>
+        <li><span onClick={() => clickHandler("/economy")} >Economy</span></li>
+      </ul>
     </div>
   )
 }
