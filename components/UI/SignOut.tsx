66a1fc6827e0631adc63847a120b81290f9047c4
Julian Pagtama <jpagtama@gmail.com>
2022-10-22 13:39:35 -0700

    Add sign out alert component

---
+++ components/UI/SignOut.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Button from './Button'
+import { signOut } from "next-auth/react"
+import styles from '@styles/UI/SignOut.module.css'
+
+interface Props {
+    closeSignOutAlert: () => void
+}
+
+const signOutHandler = () => {
+    signOut({ callbackUrl: '/' })
+}
+
+const SignOut = ({ closeSignOutAlert }: Props) => {
+    return (
+        <div className={styles.container}>
+            <h1 className={styles.title}>Are you sure you want to sign out?</h1>
+            <div className={styles.buttonsContainer}>
+                <Button onClick={closeSignOutAlert} label='Cancel' />
+                <Button onClick={signOutHandler} label='Sign me out' />
+            </div>
+        </div>
+    )
+}
+
+export default SignOut
