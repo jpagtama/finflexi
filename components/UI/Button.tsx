91e88393f6c9b7c8a14f3a4f26bcf8164febbae2
Julian Pagtama <jpagtama@gmail.com>
2022-10-22 13:35:06 -0700

    Add Button and Input components for reusability

---
+++ components/UI/Button.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+interface Props {
+    onClick: (e: React.MouseEvent<HTMLButtonElement>) => void
+    label: string
+}
+
+const Button = ({ onClick, label }: Props) => {
+    return (
+        <button onClick={onClick} style={{ padding: '1em', border: 'none', borderRadius: '5px', cursor: 'pointer', backgroundColor: 'var(--neon-green)' }} >
+            {label}
+        </button>
+    )
+}
+
+export default Button
---
+++ components/UI/Input.tsx
@@ -0,0 +1,26 @@
+import React, { useRef, forwardRef, useImperativeHandle } from 'react'
+
+interface Props {
+    value: string
+    changeHandler: (e: React.ChangeEvent<HTMLInputElement>) => void
+    id?: string
+    ref?: string
+    type?: string
+    placeholder?: string
+}
+
+const Input = forwardRef(({ value, id, changeHandler, type = 'text', placeholder = '' }: Props, ref) => {
+
+    const inputRef = useRef<HTMLInputElement>(null)
+    // const setFocus = () => {
+    //     inputRef.current?.focus()
+    // }
+    useImperativeHandle(ref, () => ({ focus: () => inputRef.current?.focus() }))
+
+    return (
+        <input ref={inputRef} id={id} type={type} value={value} onChange={changeHandler} placeholder={placeholder}
+            style={{ width: "100%", padding: "1em", border: "none", borderRadius: "5px", color: "ivory", backgroundColor: "var(--gray)" }} />
+    )
+})
+
+export default Input
