b85ad830835e949bba3e358bb14fe3937527af95
Julian Pagtama <jpagtama@gmail.com>
2022-10-23 23:49:56 -0700

    Add signin alert

--- components/UI/MenuList.tsx
+++ components/UI/MenuList.tsx
@@ -1,26 +1,46 @@
+import { useState } from 'react'
+import { useSession } from 'next-auth/react'
 import { menuActions } from '@store/index'
 import { useDispatch } from 'react-redux'
 import { useRouter } from 'next/router'
 import styles from '@styles/UI/MenuList.module.css'
-import React from 'react'
+import SignOut from './SignOut'
 
 const MenuList = () => {
+  const [displaySignOut, setDisplaySignOut] = useState(false)
   const dispatch = useDispatch()
   const router = useRouter()
+  const { data, status } = useSession()
+
+  console.log('session data :>> ', data);
+  console.log('session status :>> ', status);
 
   const clickHandler = (path: string) => {
     dispatch(menuActions.close())
     router.push(path)
   }
+  const displaySignOutAlert = () => {
+    setDisplaySignOut(true)
+  }
+  const closeSignOutAlert = () => {
+    setDisplaySignOut(false)
+  }
+  const renderMenuItems = () => {
+    return (
+      <ul className={styles.menuList}>
+        {status === 'unauthenticated' && <li><span onClick={() => clickHandler("/signin")} >Sign In</span></li>}
+        <li><span onClick={() => clickHandler("/")} >Home</span></li>
+        <li><span onClick={() => clickHandler("/company/search")} >Search</span></li>
+        <li><span onClick={() => clickHandler("/economy")} >Economy</span></li>
+        {status === 'authenticated' && <li><span onClick={displaySignOutAlert} >Sign Out</span></li>}
+      </ul>
+    )
+  }
 
   return (
     <div className={styles.container}>
-      <ul className={styles.menuList}>
-        <li><span onClick={() => clickHandler("/")} >Home</span></li>
-        <li><span onClick={() => clickHandler("/signup")} >Sign-Up</span></li>
-        <li><span onClick={() => clickHandler("/company/search")} >Search</span></li>
-        <li><span onClick={() => clickHandler("/economy")} >Economy</span></li>
-      </ul>
+      {displaySignOut && <SignOut closeSignOutAlert={closeSignOutAlert} />}
+      {!displaySignOut && renderMenuItems()}
     </div>
   )
 }
