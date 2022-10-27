78707dd5a0a3be514cac777603f47eadbd502b51
Julian Pagtama <jpagtama@gmail.com>
2022-10-27 15:41:46 -0700

    Add next-auth config

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,92 @@
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import NextAuth from 'next-auth'
+import EmailProvider from 'next-auth/providers/email'
+import { prisma } from '@db/index'
+import * as crypto from 'crypto'
+
+export default NextAuth({
+    adapter: PrismaAdapter(prisma),
+    providers: [
+        EmailProvider({
+            server: process.env.EMAIL_SERVER,
+            from: process.env.EMAIL_FROM,
+            // maxAge: 24 * 60 * 60, // How long email links are valid for (default 24h)
+        }),
+    ],
+    // Database optional. MySQL, Maria DB, Postgres and MongoDB are supported.
+    // https://next-auth.js.org/configuration/databases
+    //
+    // Notes:
+    // * You must install an appropriate node_module for your database
+    // * The Email provider requires a database (OAuth providers do not)
+    // database: process.env.DATABASE_URL,
+
+    // The secret should be set to a reasonably long random string.
+    // It is used to sign cookies and to sign and encrypt JSON Web Tokens, unless
+    // a separate secret is defined explicitly for encrypting the JWT.
+    secret: process.env.SECRET,
+    session: {
+        // Choose how you want to save the user session.
+        // The default is `"jwt"`, an encrypted JWT (JWE) stored in the session cookie.
+        // If you use an `adapter` however, we default it to `"database"` instead.
+        // You can still force a JWT session by explicitly defining `"jwt"`.
+        // When using `"database"`, the session cookie will only contain a `sessionToken` value,
+        // which is used to look up the session in the database.
+        strategy: 'jwt',
+
+        // Seconds - How long until an idle session expires and is no longer valid.
+        maxAge: 30 * 24 * 60 * 60, // 30 days
+
+        // Seconds - Throttle how frequently to write to database to extend a session.
+        // Use it to limit write operations. Set to 0 to always update the database.
+        // Note: This option is ignored if using JSON Web Tokens
+        updateAge: 24 * 60 * 60, // 24 hours
+
+        // The session token is usually either a random UUID or string, however if you
+        // need a more customized session token string, you can define your own generate function.
+        generateSessionToken: () => {
+            return crypto.randomUUID?.() ?? crypto.randomBytes(32).toString("hex")
+        }
+    },
+    jwt: {
+        // A secret to use for key generation (you should set this explicitly)
+        secret: process.env.SECRET,
+        // Set to true to use encryption (default: false)
+        // encryption: true,
+        // You can define your own encode/decode functions for signing and encryption
+        // if you want to override the default behaviour.
+        // encode: async ({ secret, token, maxAge }) => {},
+        // decode: async ({ secret, token, maxAge }) => {},
+    },
+    // You can define custom pages to override the built-in ones. These will be regular Next.js pages
+    // so ensure that they are placed outside of the '/api' folder, e.g. signIn: '/auth/mycustom-signin'
+    // The routes shown here are the default URLs that will be used when a custom
+    // pages is not specified for that route.
+    // https://next-auth.js.org/configuration/pages
+    pages: {
+        signIn: '/signin',  // Displays signin buttons
+        // signOut: '/auth/signout', // Displays form with sign out button
+        // error: '/auth/error', // Error code passed in query string as ?error=
+        // verifyRequest: '/auth/verify-request', // Used for check email page
+        // newUser: null // If set, new users will be directed here on first sign in
+    },
+    // Callbacks are asynchronous functions you can use to control what happens
+    // when an action is performed.
+    // https://next-auth.js.org/configuration/callbacks
+    callbacks: {
+        // async signIn({ user, account, profile, email, credentials }) { return true },
+        // async redirect({ url, baseUrl }) { return baseUrl },
+        // async session({ session, token, user }) { return session },
+        async session({ session, token, user }) {
+            return { ...session, userId: token.sub }
+        },
+        // async jwt({ token, user, account, profile, isNewUser }) { return token }
+    },
+
+    // Events are useful for logging
+    // https://next-auth.js.org/configuration/events
+    events: {},
+
+    // Enable debug messages in the console if you are having problems
+    debug: false,
+})
