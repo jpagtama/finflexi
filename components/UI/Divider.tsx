bb07d7fa2b8f0e63daa8dd1273de319854369ed1
Julian Pagtama <jpagtama@gmail.com>
2022-11-01 09:13:59 -0700

    Remove commented style

--- components/UI/Divider.tsx
+++ components/UI/Divider.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
 
 const styles = {
-    // background: "rgb(249,0,191)",
-    background: "linear-gradient(90deg, rgba(249,0,191,0.25) 0%, rgba(120,133,181,1) 50%, rgba(0,255,171,0.25) 100%)",
-    width: "100%",
-    height: "1px"
+  background: "linear-gradient(90deg, rgba(249,0,191,0.25) 0%, rgba(120,133,181,1) 50%, rgba(0,255,171,0.25) 100%)",
+  width: "100%",
+  height: "1px"
 }
 
 const Divider = () => {
