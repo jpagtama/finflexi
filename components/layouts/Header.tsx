d6bf755569b8bdf0d2191682110967b34dba4cd5
Julian Pagtama <jpagtama@gmail.com>
2022-11-02 08:28:47 -0700

    Add search bar to header

--- components/layouts/Header.tsx
+++ components/layouts/Header.tsx
@@ -1,18 +1,28 @@
+import { useRouter } from 'next/router'
 import Menu from '@components/UI/Menu'
 import MenuList from '@components/UI/MenuList'
+import SearchBar from '@components/UI/SearchBar'
 import { useSelector } from 'react-redux'
 import styles from '@styles/layouts/Header.module.css'
 
 
 const Header = () => {
-  const isMenuOpen = useSelector((state: {"menu":{"open": boolean}}) => state.menu.open)
+  const router = useRouter()
+  const isMenuOpen = useSelector((state: { "menu": { "open": boolean } }) => state.menu.open)
 
   return (
     <>
-    <header className={`${styles.header} ${isMenuOpen && styles.menuIsOpen}`}>
-      <Menu />
-      {isMenuOpen && <MenuList />}
-    </header>
+      <header className={`${styles.header} ${isMenuOpen && styles.menuIsOpen}`}>
+        <div className={styles.headerSection} >
+          <span className={styles.searchBarContainer}>
+            {(router.asPath !== '/' && !isMenuOpen) && <SearchBar />}
+          </span>
+          <span className={styles.menuIcon}>
+            <Menu />
+          </span>
+        </div>
+        {isMenuOpen && <MenuList />}
+      </header>
     </>
   )
 }
