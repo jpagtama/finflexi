371d323efbcbee19acbd5014aed220f661e71222
Julian Pagtama <jpagtama@gmail.com>
2022-11-04 09:28:07 -0700

    Format homepage and reinforce intro animation condition

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,3 @@
-import { GetStaticPropsContext } from 'next'
 import { useState, useEffect } from 'react'
 import { useSession } from 'next-auth/react'
 import Router from 'next/router'
@@ -6,6 +5,7 @@ import Head from 'next/head'
 import AppIntro from '@components/landing/AppIntro'
 import SearchBar from '@components/UI/SearchBar'
 import styles from '@styles/landing/Index.module.css'
+import Link from 'next/link'
 
 interface CompanyList {
   symbol: string
@@ -16,8 +16,16 @@ const Home = () => {
   const initialCompanies: Array<CompanyList> = []
   const { status: sessionStatus } = useSession()
 
-  const [animateIntro, setAnimateIntro] = useState(sessionStatus !== 'authenticated')
+  const [animateIntro, setAnimateIntro] = useState(false)
   const [companyResults, setCompanyResults] = useState(initialCompanies)
+
+  useEffect(() => {
+    if (sessionStatus === 'unauthenticated' && !window.sessionStorage.getItem("visted")) {
+      // Do if user is not signed in and its their first time getting to the page
+      window.sessionStorage.setItem('visted', '1')
+      setAnimateIntro(true)
+    }
+  }, [])
 
   useEffect(() => {
     if (companyResults.length > 0) {
@@ -38,6 +46,10 @@ const Home = () => {
     setCompanyResults(companies)
   }
 
+  const signUpHandler = () => {
+
+  }
+
   return (
     <>
       <Head>
@@ -45,15 +57,30 @@ const Home = () => {
         <meta name="description" content="Access to stock market data and technical analysis" />
       </Head>
       {animateIntro && <AppIntro introDoneHandler={introDoneHandler} endIntroHandler={endIntroHandler} />}
-      <section className={styles.searchSection}>
-        <h1 className={styles.sloganHeading}>Flexibility in Finance. FinFlexi.</h1>
+      <section className={styles.bannerSection}>
+        <Link href='/signin'><button className={styles.signUpButton}>Sign Up!</button></Link>
+      </section>
+      <section className={styles.searchBarContainer}>
+        <p className={styles.searchDesc}>Get started right away:</p>
         <SearchBar searchCompaniesHandler={searchCompaniesHandler} />
       </section>
-      <section>
-        <h1>Laser focused.</h1>
-        <h2>Get access to stock market data, company statistics, and more.</h2>
-        <h1>Straight to the point.</h1>
-        <h2>Key economic indicators helps understand where your investments are going.</h2>
+      <section className={styles.sectionContainer}>
+        <div className={styles.sectionDetails}>
+          <h1>Laser focused.</h1>
+          <p>Get access to stock market data, company statistics, and more.</p>
+        </div>
+        <div className={styles.sectionImage}>
+
+        </div>
+      </section>
+      <section className={styles.sectionContainer}>
+        <div className={styles.sectionDetails}>
+          <h1>Straight to the point.</h1>
+          <p>Key economic indicators helps understand where your investments are going.</p>
+        </div>
+        <div className={styles.sectionImage}>
+
+        </div>
       </section>
     </>
   )
