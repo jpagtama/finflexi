f6ee72a6c429157191ee918c47268e143aa37d0f
Julian Pagtama <jpagtama@gmail.com>
2022-11-07 20:00:15 -0800

    Add custom verifyRequest option to nextauth.ts and modify html being sent

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,7 +2,49 @@ import { PrismaAdapter } from '@next-auth/prisma-adapter'
 import NextAuth from 'next-auth'
 import EmailProvider from 'next-auth/providers/email'
 import { prisma } from '@db/index'
+import { createTransport } from 'nodemailer'
 import * as crypto from 'crypto'
+
+const html = (params: { url: string; host: string; }) => {
+    const { url, host } = params
+    const escapedHost = host.replace(/\./g, "&#8203;.")
+
+    return `
+        <body style="background: none;">
+        <table width="100%" border="0" cellspacing="20" cellpadding="0"
+        style="background: black; max-width: 600px; margin: auto; border-radius: 10px;">
+        <tr>
+            <td align="center"
+            style="padding: 10px 0px; font-size: 22px; font-family: Helvetica, Arial, sans-serif; color: #F900BF;">
+            Sign in to <strong>${escapedHost}</strong>
+            </td>
+        </tr>
+        <tr>
+            <td align="center" style="padding: 20px 0;">
+            <table border="0" cellspacing="0" cellpadding="0">
+                <tr>
+                <td align="center" style="border-radius: 5px;" bgcolor="#293462"><a href="${url}"
+                    target="_blank"
+                    style="font-size: 18px; font-weight: 300; font-family: Helvetica, Arial, sans-serif; color: white; text-decoration: none; border-radius: 55px; padding: 10px 20px; border: none; display: inline-block; font-weight: bold;">Sign
+                    in</a></td>
+                </tr>
+            </table>
+            </td>
+        </tr>
+        <tr>
+            <td align="center"
+            style="padding: 0px 0px 10px 0px; font-size: 16px; line-height: 22px; font-family: Helvetica, Arial, sans-serif; color: #F900BF;">
+            If you did not request this email you can safely ignore it.
+            </td>
+        </tr>
+        </table>
+    </body>
+    `
+}
+
+const text = ({ url, host }: { url: string; host: string }) => {
+    return `Sign in to ${host}\n${url}\n\n`
+}
 
 export default NextAuth({
     adapter: PrismaAdapter(prisma),
@@ -10,6 +52,26 @@ export default NextAuth({
         EmailProvider({
             server: process.env.EMAIL_SERVER,
             from: process.env.EMAIL_FROM,
+            async sendVerificationRequest({
+                identifier: email,
+                url,
+                provider: { server, from },
+            }) {
+                const { host } = new URL(url)
+                // NOTE: You are not required to use `nodemailer`, use whatever you want.
+                const transport = createTransport(server)
+                const result = await transport.sendMail({
+                    to: email,
+                    from: from,
+                    subject: `Sign in to ${host}`,
+                    text: text({ url, host }),
+                    html: html({ url, host }),
+                })
+                const failed = result.rejected.concat(result.pending).filter(Boolean)
+                if (failed.length) {
+                    throw new Error(`Email(s) (${failed.join(", ")}) could not be sent`)
+                }
+            },
             // maxAge: 24 * 60 * 60, // How long email links are valid for (default 24h)
         }),
     ],
@@ -67,7 +129,7 @@ export default NextAuth({
         signIn: '/signin',  // Displays signin buttons
         // signOut: '/auth/signout', // Displays form with sign out button
         // error: '/auth/error', // Error code passed in query string as ?error=
-        // verifyRequest: '/auth/verify-request', // Used for check email page
+        verifyRequest: '/verify-request', // Used for check email page
         // newUser: null // If set, new users will be directed here on first sign in
     },
     // Callbacks are asynchronous functions you can use to control what happens
