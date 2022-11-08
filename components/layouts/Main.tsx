4a119590e19d2dd8ec87b518c8cbc4a70864a861
Julian Pagtama <jpagtama@gmail.com>
2022-11-07 19:57:48 -0800

    Remove header if verify-request page is shown

--- components/layouts/Main.tsx
+++ components/layouts/Main.tsx
@@ -1,14 +1,17 @@
 import Footer from '@components/layouts/Footer'
 import Header from '@components/layouts/Header'
+import { useRouter } from 'next/router'
 
 interface Props {
   children: React.ReactNode
 }
 
 const Main = (props: Props) => {
+  const router = useRouter()
+
   return (
     <main id="main">
-      <Header />
+      {router.asPath !== '/verify-request' && <Header />}
       {props.children}
       <Footer />
     </main>
