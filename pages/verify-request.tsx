2327276a8c4a6ea4f6244b2981f77437ce67aa9e
Julian Pagtama <jpagtama@gmail.com>
2022-11-07 19:58:59 -0800

    Add verify-request page

---
+++ pages/verify-request.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { MdOutlineMarkEmailUnread } from 'react-icons/md'
+import { IconContext } from 'react-icons'
+import styles from '@styles/VerifyRequest.module.css'
+
+const VerifyRequest = () => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.header}></div>
+            <h1 className={styles.title}>Check your Email</h1>
+            <h2>You're almost there!</h2>
+            <div className={styles.mailIcon}>
+                <IconContext.Provider value={{ size: '3em' }}>
+                    <MdOutlineMarkEmailUnread />
+                </IconContext.Provider>
+            </div>
+            <p>Click the sign-in link we sent you and you're ready to go</p>
+        </div>
+    )
+}
+
+export default VerifyRequest
