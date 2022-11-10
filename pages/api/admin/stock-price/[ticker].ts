9da4ef86f0a474a470527965db36e1648a6f7db9
Julian Pagtama <jpagtama@gmail.com>
2022-11-09 17:07:17 -0800

    Bug: change update stock prices API to use daily adjusted and add error checking

--- pages/api/admin/stock-price/[ticker].ts
+++ pages/api/admin/stock-price/[ticker].ts
@@ -1,45 +1,58 @@
-import {prisma} from '@db/index'
+import { prisma } from '@db/index'
 import type { NextApiRequest, NextApiResponse } from "next"
 
 const updateStockPrices = async (ticker: string) => {
-  const response = await fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${ticker}&apikey=${process.env.ALPHAVANTAGE_APIKEY}`)
-  const data = await response.json()
-  const dailyData = {...data['Time Series (Daily)']}
 
-  let prices = []
-  for (const i in dailyData) {
-    const date = new Date(i)
+  try {
+    const response = await fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${ticker}&apikey=${process.env.ALPHAVANTAGE_APIKEY}`)
+    const data = await response.json()
 
-    prices.push({
-      companyticker: ticker, 
-      date: date, 
-      open: parseFloat(dailyData[i]['1. open']), 
-      close: parseFloat(dailyData[i]['4. close']), 
-      high: parseFloat(dailyData[i]['2. high']), 
-      low: parseFloat(dailyData[i]['3. low']), 
-      volume: parseInt(dailyData[i]['5. volume'])
-    })
+    // console.log(`${ticker} data from stock prices`, data);
+
+    const dailyData = { ...data['Time Series (Daily)'] }
+    if (dailyData === undefined) throw new Error('data is unavailable')
+
+    let prices = []
+    for (const i in dailyData) {
+      const date = new Date(i)
+
+      prices.push({
+        companyticker: ticker,
+        date: date,
+        open: parseFloat(dailyData[i]['1. open']),
+        close: parseFloat(dailyData[i]['4. close']),
+        high: parseFloat(dailyData[i]['2. high']),
+        low: parseFloat(dailyData[i]['3. low']),
+        volume: parseInt(dailyData[i]['6. volume'])
+      })
+    }
+
+    prisma.$transaction([
+      prisma.stock_data_daily.deleteMany({ where: { companyticker: ticker } }),
+      prisma.stock_data_daily.createMany({
+        data: prices
+      })
+    ])
+
+    return true
+  } catch (e) {
+    if (e instanceof Error) console.log(e.message)
+    return false
   }
 
-  prisma.$transaction([
-    prisma.stock_data_daily.deleteMany({where: {companyticker: ticker}}),
-    prisma.stock_data_daily.createMany({
-      data: prices
-    })
-  ])
 }
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const ticker = req.query.ticker
 
-  if (typeof ticker !== 'string') return res.status(400).json({status: {success: false, message:'bad request'}})
+  if (typeof ticker !== 'string') return res.status(400).json({ status: { success: false, message: 'bad request' } })
 
   try {
-    updateStockPrices(ticker)
-    return res.status(200).json({status: {success: true, message: 'ok'}})
-  } catch(error) {
-    if (error instanceof Error) return res.status(500).json({status: {success: false, message: error.message}})
-    return res.status(500).json({status: {success: false, message: 'unable to process'}})
+    const successful = await updateStockPrices(ticker)
+    return res.status(200).json({ status: { success: successful, message: 'ok' } })
+  } catch (error) {
+    if (error instanceof Error) return res.status(500).json({ status: { success: false, message: error.message } })
+    return res.status(500).json({ status: { success: false, message: 'unable to process' } })
   }
 }
 
