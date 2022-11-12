7152f28cc14d4bd49c20a42d883afe824cec7f46
Julian Pagtama <jpagtama@gmail.com>
2022-11-12 11:32:38 -0800

    Add props to determine if month is currently viewed, current day indication, and events

--- components/UI/Calendar.tsx
+++ components/UI/Calendar.tsx
@@ -3,7 +3,9 @@ import styles from './Calendar.module.css'
 
 interface DayProps {
     number: number
-    events?: string[]
+    currentMonth: boolean
+    isToday: boolean
+    events: string[]
 }
 
 interface Props {
@@ -30,18 +32,33 @@ const Calendar = ({ month, year, events }: Props) => {
     const prevMonth = new Date(`${month - 1 > 0 ? year : year - 1}/${month - 1 > 0 ? month - 1 : 12}/01`) // returns the date of previous month
     const prevMonthLastDay = new Date(prevMonth.getFullYear(), prevMonth.getMonth() + 1, 0).getDate() // returns some int from 1-31
 
+    // console.log('today.getDate()', today.getDate())
+    // Get only events within the current month
+    let currentEvents: { [key: string]: string[] } = {} // store the events with day number as key and events [] as the value
+    if (events) {
+        for (const i of events) {
+            if (i.date.getMonth() + 1 === month) {
+                currentEvents[`${i.date.getDate()}`] = i.event
+            }
+        }
+    }
+
     // Set up the array of days with all events and numbering
     let days: DayProps[] = []
     let nextMonthDay = 1
     let prevMonthDayStart = prevMonthLastDay - weekDayStart + 1
     let d = 1 // calendar day counter
     for (let i = 0; i < 35; i++) {
-        if (i < weekDayStart) { days.push({ number: prevMonthDayStart++ }) }
-        else if (i === weekDayStart) { days.push({ number: d++ }) }
-        else if (i > weekDayStart && d <= lastDay) {
-            days.push({ number: d++ })
-        } else { // i > lastDay
-            days.push({ number: nextMonthDay++ })
+        if (i < weekDayStart) { days.push({ number: prevMonthDayStart++, currentMonth: false, isToday: false, events: [] }) }
+        else if (i >= weekDayStart && d <= lastDay) {
+            days.push({
+                isToday: today.getDate() !== d ? false : true,
+                currentMonth: true,
+                events: !currentEvents[d]?.length ? [] : currentEvents[d],
+                number: d++,
+            })
+        } else { // for days i after lastDay
+            days.push({ number: nextMonthDay++, currentMonth: false, isToday: false, events: [] })
         }
     }
 
@@ -73,9 +90,13 @@ const Calendar = ({ month, year, events }: Props) => {
         let result = []
         let week = []
         for (let i = 0; i < days.length; i++) {
-            if ((i + 1) % 7 !== 0) week.push(<Day key={`${i}_day`} number={days[i].number} events={days[i].events} />)
+            if ((i + 1) % 7 !== 0) week.push(
+                <Day key={`${i}_day`} number={days[i].number} currentMonth={days[i].currentMonth} isToday={days[i].isToday} events={days[i].events} />
+            )
             else {
-                week.push(<Day key={`${i}_day`} number={days[i].number} events={days[i].events} />)
+                week.push(
+                    <Day key={`${i}_day`} number={days[i].number} currentMonth={days[i].currentMonth} isToday={days[i].isToday} events={days[i].events} />
+                )
                 result.push(
                     <div key={`${i}_week`} className={styles.row}>
                         {week}
@@ -87,12 +108,15 @@ const Calendar = ({ month, year, events }: Props) => {
         return result
     }
 
-    const Day = ({ number, events }: DayProps) => {
+    const Day = ({ number, currentMonth, isToday, events }: DayProps) => {
+        const eventItems = events?.map((i, idx) => <li key={idx}>{i}</li>)
+
         return (
             <div className={styles.dayContainer}>
-                <span className={styles.dayNumber}>
+                <span className={`${styles.dayNumber} ${!isToday ? '' : styles.isToday}`}>
                     {number}
                 </span>
+                {eventItems.length ? <ul className={styles.eventListContainer}>{eventItems}</ul> : ''}
             </div>
         )
     }
