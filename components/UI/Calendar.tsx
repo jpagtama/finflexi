8c60d2294aa693265ddf3f4ffd4ee77934b92891
Julian Pagtama <jpagtama@gmail.com>
2022-11-12 09:18:06 -0800

    Style: refactor using css module

--- components/UI/Calendar.tsx
+++ components/UI/Calendar.tsx
@@ -1,4 +1,5 @@
 import React, { ReactPropTypes } from "react"
+import styles from './Calendar.module.css'
 
 interface DayProps {
     number: number
@@ -29,8 +30,6 @@ const Calendar = ({ month, year, events }: Props) => {
     const prevMonth = new Date(`${month - 1 > 0 ? year : year - 1}/${month - 1 > 0 ? month - 1 : 12}/01`) // returns the date of previous month
     const prevMonthLastDay = new Date(prevMonth.getFullYear(), prevMonth.getMonth() + 1, 0).getDate() // returns some int from 1-31
 
-    console.log('prevMonthLastDay :>> ', prevMonthLastDay);
-
     // Set up the array of days with all events and numbering
     let days: DayProps[] = []
     let nextMonthDay = 1
@@ -45,12 +44,9 @@ const Calendar = ({ month, year, events }: Props) => {
             days.push({ number: nextMonthDay++ })
         }
     }
-    // console.log('days array length', days.length)
-    // console.log('days array :>>', days)
-
 
     const renderMonthTitle = (month: string, year: string) => {
-        return <h1 style={{ color: 'black', padding: '0.25em' }}>{month} <span style={{ fontWeight: '300' }}>{year}</span></h1>
+        return <h1 className={styles.monthTitle}>{month} <span className={styles.year}>{year}</span></h1>
     }
 
     const renderWeekHeader = () => {
@@ -58,28 +54,16 @@ const Calendar = ({ month, year, events }: Props) => {
         const dayNames = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
         const arrDays = dayNames.map((i, idx) => {
             return (
-                <div key={idx} style={{
-                    flex: 1,
-                    display: 'flex',
-                    justifyContent: 'flex-end',
-                    alignItems: 'flex-end',
-                    color: 'black'
-                }}>
-                    <span style={{
-                        padding: '0.5em'
-                    }}>
+                <div key={idx} className={styles.weekdayContainer}>
+                    <div className={styles.weekdayTitle}>
                         {i}
-                    </span>
+                    </div>
                 </div>
             )
         })
 
         return (
-            <div style={{
-                display: 'flex',
-                width: '100%',
-                height: '3em'
-            }}>
+            <div className={styles.weekdayRow}>
                 {arrDays}
             </div>
         )
@@ -93,15 +77,7 @@ const Calendar = ({ month, year, events }: Props) => {
             else {
                 week.push(<Day key={`${i}_day`} number={days[i].number} events={days[i].events} />)
                 result.push(
-                    <div key={`${i}_week`} style={{
-                        display: 'flex',
-                        flexDirection: 'row',
-                        flexWrap: 'nowrap',
-                        flex: 1,
-                        borderTop: '1px solid black',
-                        borderBottom: '1px solid black',
-                        width: '100%',
-                    }}>
+                    <div key={`${i}_week`} className={styles.row}>
                         {week}
                     </div>
                 )
@@ -113,21 +89,8 @@ const Calendar = ({ month, year, events }: Props) => {
 
     const Day = ({ number, events }: DayProps) => {
         return (
-            <div style={{
-                flex: 1,
-                display: 'flex',
-                flexDirection: 'column',
-                borderBottom: 'none',
-                borderLeft: '1px solid black',
-                borderRight: '1px solid black',
-                backgroundColor: 'gray',
-                overflow: 'hidden'
-            }}>
-                <span style={{
-                    padding: '5px',
-                    alignSelf: 'flex-end',
-                    color: 'black'
-                }}>
+            <div className={styles.dayContainer}>
+                <span className={styles.dayNumber}>
                     {number}
                 </span>
             </div>
@@ -135,18 +98,7 @@ const Calendar = ({ month, year, events }: Props) => {
     }
 
     return (
-        <div style={{
-            display: 'flex',
-            justifyContent: 'center',
-            flexDirection: 'column',
-            paddingTop: '1em',
-            minWidth: '960px',
-            minHeight: '540px',
-            backgroundColor: 'brown',
-            borderRadius: '10px',
-            border: '1px solid black',
-            overflow: 'hidden'
-        }} >
+        <div className={styles.container} >
             {renderMonthTitle(monthName, year.toString())}
             {renderWeekHeader()}
             {renderTiles()}
