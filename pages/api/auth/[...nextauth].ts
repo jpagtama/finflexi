184dbd99c0ced561360bb1e27ada196b41e6af4c
Julian Pagtama <jpagtama@gmail.com>
2022-11-14 07:17:43 -0800

    Rename verify-request page to verify

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -129,7 +129,7 @@ export default NextAuth({
         signIn: '/signin',  // Displays signin buttons
         // signOut: '/auth/signout', // Displays form with sign out button
         // error: '/auth/error', // Error code passed in query string as ?error=
-        verifyRequest: '/verify-request', // Used for check email page
+        verifyRequest: '/verify', // Used for check email page
         // newUser: null // If set, new users will be directed here on first sign in
     },
     // Callbacks are asynchronous functions you can use to control what happens
