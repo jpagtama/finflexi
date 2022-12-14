3fb6527baee9e29c2b1a6b91698817864094267a
Julian Pagtama <jpagtama@gmail.com>
2022-12-14 11:11:35 -0800

    Revert to upsert when updating company overview

--- pages/api/admin/company/[ticker].ts
+++ pages/api/admin/company/[ticker].ts
@@ -12,8 +12,23 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
         const response = await fetch(`https://www.alphavantage.co/query?function=OVERVIEW&symbol=${ticker}&apikey=${process.env.ALPHAVANTAGE_APIKEY}`)
         const data = await response.json()
 
-        await prisma.companies.create({
-            data: {
+        await prisma.companies.upsert({
+            where: {
+                ticker: ticker
+            },
+            update: {
+                name: data['Name'],
+                description: data['Description'],
+                exchange: data['Exchange'],
+                marketcap: isNaN(parseInt(data['MarketCapitalization'])) ? null : parseInt(data['MarketCapitalization']),
+                analysttargetprice: isNaN(parseFloat(data['AnalystTargetPrice'])) ? null : parseFloat(data['AnalystTargetPrice']).toFixed(2),
+                sharesoutstanding: isNaN(parseInt(data['SharesOutstanding'])) ? null : parseInt(data['SharesOutstanding']),
+                forwardpe: isNaN(data['ForwardPE']) ? null : parseFloat(data['ForwardPE']).toFixed(2),
+                movingavg50: isNaN(parseFloat(data['50DayMovingAverage'])) ? null : parseFloat(data['50DayMovingAverage']).toFixed(2),
+                movingavg200: isNaN(parseFloat(data['200DayMovingAverage'])) ? null : parseFloat(data['200DayMovingAverage']).toFixed(2),
+                fiscalyearend: data['FiscalYearEnd'],
+            },
+            create: {
                 ticker: ticker,
                 name: data['Name'],
                 description: data['Description'],
@@ -27,43 +42,10 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
                 fiscalyearend: data['FiscalYearEnd'],
             }
         })
-
-        return data
-
-        // await prisma.companies.upsert({
-        //     where: {
-        //         ticker: ticker
-        //     },
-        //     update: {
-        //         name: data['Name'],
-        //         description: data['Description'],
-        //         exchange: data['Exchange'],
-        //         marketcap: isNaN(parseInt(data['MarketCapitalization'])) ? null : parseInt(data['MarketCapitalization']),
-        //         analysttargetprice: isNaN(parseFloat(data['AnalystTargetPrice'])) ? null : parseFloat(data['AnalystTargetPrice']).toFixed(2),
-        //         sharesoutstanding: isNaN(parseInt(data['SharesOutstanding'])) ? null : parseInt(data['SharesOutstanding']),
-        //         forwardpe: isNaN(data['ForwardPE']) ? null : parseFloat(data['ForwardPE']).toFixed(2),
-        //         movingavg50: isNaN(parseFloat(data['50DayMovingAverage'])) ? null : parseFloat(data['50DayMovingAverage']).toFixed(2),
-        //         movingavg200: isNaN(parseFloat(data['200DayMovingAverage'])) ? null : parseFloat(data['200DayMovingAverage']).toFixed(2),
-        //         fiscalyearend: data['FiscalYearEnd'],
-        //     },
-        //     create: {
-        //         ticker: ticker,
-        //         name: data['Name'],
-        //         description: data['Description'],
-        //         exchange: data['Exchange'],
-        //         marketcap: isNaN(parseInt(data['MarketCapitalization'])) ? null : parseInt(data['MarketCapitalization']),
-        //         analysttargetprice: isNaN(parseFloat(data['AnalystTargetPrice'])) ? null : parseFloat(data['AnalystTargetPrice']).toFixed(2),
-        //         sharesoutstanding: isNaN(parseInt(data['SharesOutstanding'])) ? null : parseInt(data['SharesOutstanding']),
-        //         forwardpe: isNaN(data['ForwardPE']) ? null : parseFloat(data['ForwardPE']).toFixed(2),
-        //         movingavg50: isNaN(parseFloat(data['50DayMovingAverage'])) ? null : parseFloat(data['50DayMovingAverage']).toFixed(2),
-        //         movingavg200: isNaN(parseFloat(data['200DayMovingAverage'])) ? null : parseFloat(data['200DayMovingAverage']).toFixed(2),
-        //         fiscalyearend: data['FiscalYearEnd'],
-        //     }
-        // })
     }
 
     try {
-        const message = updateCompanyOverview(ticker)
+        updateCompanyOverview(ticker)
         return res.status(200).json({ status: { success: success, message: message } })
     } catch (error) {
         success = false
