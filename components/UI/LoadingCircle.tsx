393c1aa6505969b5a2ddc593220029a8ce83a20f
Julian Pagtama <jpagtama@gmail.com>
2022-12-15 20:35:20 -0800

    Add small loading circle component

---
+++ components/UI/LoadingCircle.tsx
@@ -0,0 +1,9 @@
+import styles from '@styles/UI/LoadingCircle.module.css'
+
+const LoadingCircle = () => {
+    return (
+        <div className={styles.ldsDefault}><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+    )
+}
+
+export default LoadingCircle
