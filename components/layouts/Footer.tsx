6809ecb60213fa79e1882984ffbc20d6308bccc0
Julian Pagtama <jpagtama@gmail.com>
2022-12-20 11:51:59 -0800

    Add footer links

--- components/layouts/Footer.tsx
+++ components/layouts/Footer.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Divider from '../UI/Divider'
 import styles from '../../styles/layouts/Footer.module.css'
+import Link from 'next/link'
 
 const Footer = () => {
   const year = new Date().getFullYear()
@@ -10,9 +11,8 @@ const Footer = () => {
         <Divider />
         <ul>
           <li className={styles.listTitle}>Company</li>
-          <li>About</li>
-          <li>API</li>
-          <li>View the code</li>
+          <li><Link href='/about'>About</Link></li>
+          <li><a href='https://github.com/jpagtama/finflexi' target='_blank'>View the code</a></li>
         </ul>
         <p className={styles.disclaimer}>
           All data provided on Finflexi is provided for informational purposes only, and is not intended for trading or investing purposes. Stock prices displayed in the ticker are from a subset of exchanges, this price does not represent the real-time price.
