656d100729c527e1131c32f63a1c279fca119e08
Julian Pagtama <jpagtama@gmail.com>
2022-12-28 13:34:08 -0800

    Add set up to SearchBar test

--- __tests__/SearchBar.test.tsx
+++ __tests__/SearchBar.test.tsx
@@ -2,14 +2,36 @@
  * @jest-environment jsdom
  */
 
+import { rest } from 'msw'
+import { setupServer } from 'msw/node'
 import { fireEvent, render, screen, waitFor } from '@testing-library/react'
 import SearchBar from '../components/UI/SearchBar'
 import '@testing-library/jest-dom'
 
-describe('SearchBar', () => {
 
-  beforeEach(() => {
-    waitFor(() => render(<SearchBar />))
+describe('SearchBar', () => {
+  const server = setupServer(
+    rest.get('/api/companies', (req, res, ctx) => {
+      return res(ctx.json({
+        data: [
+          { ticker: 'AAPL', name: 'Apple Inc' },
+          { ticker: 'NFLX', name: 'Netflix Inc' },
+          { ticker: 'TSLA', name: 'Tesla Inc' },
+        ],
+        status: {
+          success: true,
+          message: 'ok'
+        }
+      }))
+    })
+  )
+
+  beforeAll(() => server.listen())
+  afterEach(() => server.resetHandlers())
+  afterAll(() => server.close())
+
+  beforeEach(async () => {
+    await waitFor(async () => render(<SearchBar />))
   })
 
   it('renders the input box for company search', () => {
