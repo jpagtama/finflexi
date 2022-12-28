fc761938716edf67594604e928a41a1098d41a27
Julian Pagtama <jpagtama@gmail.com>
2022-12-27 16:22:37 -0800

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,31 @@
 module.exports = {
+    collectCoverage: true,
+    // on node 14.x coverage provider v8 offers good speed and more or less good report
+    coverageProvider: 'v8',
+    collectCoverageFrom: [
+        '**/*.{js,jsx,ts,tsx}',
+        '!**/*.d.ts',
+        '!**/node_modules/**',
+        '!<rootDir>/out/**',
+        '!<rootDir>/.next/**',
+        '!<rootDir>/*.config.js',
+        '!<rootDir>/coverage/**',
+    ],
     setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
     testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
     moduleNameMapper: {
-        '\\.(scss|sass|css)$': 'identity-obj-proxy'
-    }
+        '\\.(scss|sass|css)$': 'identity-obj-proxy',
+        '\\.(png|jpg|jpeg|gif|webp|avif|ico|bmp|svg)': `<rootDir>/__mocks__/fileMock.js`,
+        '@components/(.*)': '<rootDir>/components/$1',
+    },
+    testEnvironment: 'jsdom',
+    transform: {
+        // Use babel-jest to transpile tests with the next/babel preset
+        // https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object
+        '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
+    },
+    transformIgnorePatterns: [
+        '/node_modules/',
+        '^.+\\.module\\.(css|sass|scss)$',
+    ],
 };
