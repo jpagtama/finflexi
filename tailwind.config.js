b6485d8b52436ef2f9940c2ccf349e4c0c34d52b
Julian Pagtama <jpagtama@gmail.com>
2023-11-04 08:25:29 -0700

    Add custom text and background color to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,14 @@ module.exports = {
   purge: ["./pages/**/*.{html,js,ts,tsx}", "./components/**/*.{html,js,ts,tsx}"],
   theme: {
     extend: {
-      backgroundColor: {
+      textColor: {
+        'light-indigo': '#A5B4FC',
         'dirty-white': '#DFE0E1'
-      }
+      },
+      backgroundColor: {
+        'dirty-white': '#DFE0E1',
+        'light-indigo': '#A5B4FC',
+      },
     },
   },
   plugins: [],
