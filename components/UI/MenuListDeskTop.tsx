f62251b491061c6c668f213ab1b519e68d28b091
Julian Pagtama <jpagtama@gmail.com>
2023-11-08 10:20:17 -0800

    Change menuListDeskTop to use isLoggedIn from props

--- components/UI/MenuListDeskTop.tsx
+++ components/UI/MenuListDeskTop.tsx
@@ -1,11 +1,15 @@
-import { useSession } from 'next-auth/react';
 import { menuActions } from '@store/index';
 import { useDispatch, useSelector } from 'react-redux';
 import { useRouter } from 'next/router'
+import Loading from './Loading';
 
-const MenuListDeskTop = () => {
+interface Props {
+    isLoggedIn: boolean;
+    isLoadingUserInfo: boolean;
+}
+
+const MenuListDeskTop = ({ isLoggedIn, isLoadingUserInfo }: Props) => {
     const isSignOutOpen = useSelector((state: { "menu": { "signOutOpen": boolean } }) => state.menu.signOutOpen)
-    const { status } = useSession();
     const router = useRouter();
     const dispatch = useDispatch();
 
@@ -23,17 +27,18 @@ const MenuListDeskTop = () => {
 
 
     return (
-        <nav className='hidden sm:block'>
-            <ul className='flex sm:gap-8 text-dirty-white'>
-                {status === 'unauthenticated' && <li className='hover:cursor-pointer hover:scale-110 duration-150'><span onClick={() => clickHandler("/")} >Home</span></li>}
-                {status === 'authenticated' && <li className='hover:cursor-pointer hover:scale-110 duration-150'><span onClick={() => clickHandler("/dashboard")} >Dashboard</span></li>}
-                <li className='hover:cursor-pointer hover:scale-110 duration-150'><span onClick={() => clickHandler("/dashboard/favorites")} >My Favorites</span></li>
-                <li className='hover:cursor-pointer hover:scale-110 duration-150'><span onClick={() => clickHandler("/economy")} >Economic Indicators</span></li>
-                {status === 'unauthenticated' && <li className='hover:cursor-pointer hover:scale-110 duration-150'><span onClick={() => clickHandler("/about")} >About Us</span></li>}
-                {status === 'authenticated' && <li className='hover:cursor-pointer hover:scale-110 duration-150'><span onClick={() => clickHandler("/account")} >My Account</span></li>}
-                {status === 'unauthenticated' && <li className='hover:cursor-pointer hover:scale-110 duration-150'><span onClick={() => clickHandler("/signin")} >Sign In</span></li>}
-                {status === 'authenticated' && <li className='hover:cursor-pointer hover:scale-110 duration-150'><span onClick={displaySignOutAlert} >Sign Out</span></li>}
-            </ul>
+        <nav >
+            {isLoadingUserInfo && <Loading />}
+            {!isLoadingUserInfo && <ul className='flex sm:gap-8 text-dirty-white'>
+                {<li className='hover:cursor-pointer hover:scale-110 hover:text-white duration-150'><span onClick={() => clickHandler("/")} >Home</span></li>}
+                {isLoggedIn && <li className='hover:cursor-pointer hover:scale-110 hover:text-white duration-150'><span onClick={() => clickHandler("/dashboard")} >Dashboard</span></li>}
+                {isLoggedIn && <li className='hover:cursor-pointer hover:scale-110 hover:text-white duration-150'><span onClick={() => clickHandler("/dashboard/favorites")} >My Favorites</span></li>}
+                <li className='hover:cursor-pointer hover:scale-110 hover:text-white duration-150'><span onClick={() => clickHandler("/economy")} >Economic Indicators</span></li>
+                {<li className='hover:cursor-pointer hover:scale-110 hover:text-white duration-150'><span onClick={() => clickHandler("/about")} >About Us</span></li>}
+                {isLoggedIn && <li className='hover:cursor-pointer hover:scale-110 hover:text-white duration-150'><span onClick={() => clickHandler("/account")} >My Account</span></li>}
+                {!isLoggedIn && <li className='hover:cursor-pointer hover:scale-110 hover:text-white duration-150'><span onClick={() => clickHandler("/signin")} >Sign In</span></li>}
+                {isLoggedIn && <li className='hover:cursor-pointer hover:scale-110 hover:text-white duration-150'><span onClick={displaySignOutAlert} >Sign Out</span></li>}
+            </ul>}
         </nav>
     )
 }
