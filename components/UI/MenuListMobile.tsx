51430f91cea87bcee50a35cacf5a9daab9e9f4af
Julian Pagtama <jpagtama@gmail.com>
2023-11-08 10:18:50 -0800

    Format menu for mobile

--- components/UI/MenuListMobile.tsx
+++ components/UI/MenuListMobile.tsx
@@ -1,15 +1,18 @@
-import { useSession } from 'next-auth/react'
-import { menuActions } from '@store/index'
-import { useDispatch, useSelector } from 'react-redux'
-import { useRouter } from 'next/router'
-import SignOut from './SignOut'
-import styles from '@styles/UI/MenuList.module.css'
+import { menuActions } from '@store/index';
+import { RootState } from '@store/index';
+import { useDispatch, useSelector } from 'react-redux';
+import { useRouter } from 'next/router';
+import SignOut from './SignOut';
+import styles from '@styles/UI/MenuList.module.css';
 
-const MenuListMobile = () => {
-  const isSignOutOpen = useSelector((state: { "menu": { "signOutOpen": boolean } }) => state.menu.signOutOpen)
+interface Props {
+  isLoggedIn: boolean
+}
+
+const MenuListMobile = ({ isLoggedIn }: Props) => {
+  const { signOutOpen } = useSelector((state: RootState) => state.menu);
   const dispatch = useDispatch()
   const router = useRouter()
-  const { data, status } = useSession()
 
   const clickHandler = (path: string) => {
     dispatch(menuActions.close())
@@ -23,23 +26,25 @@ const MenuListMobile = () => {
   }
   const renderMenuItems = () => {
     return (
-      <ul className={styles.menuList}>
-        {status === 'unauthenticated' && <li><span onClick={() => clickHandler("/")} >Home</span></li>}
-        {status === 'authenticated' && <li><span onClick={() => clickHandler("/dashboard")} >Dashboard</span></li>}
-        <li><span onClick={() => clickHandler("/dashboard/favorites")} >My Favorites</span></li>
-        <li><span onClick={() => clickHandler("/economy")} >Economic Indicators</span></li>
-        {status === 'unauthenticated' && <li><span onClick={() => clickHandler("/about")} >About Us</span></li>}
-        {status === 'authenticated' && <li><span onClick={() => clickHandler("/account")} >My Account</span></li>}
-        {status === 'unauthenticated' && <li><span onClick={() => clickHandler("/signin")} >Sign In</span></li>}
-        {status === 'authenticated' && <li><span onClick={displaySignOutAlert} >Sign Out</span></li>}
-      </ul>
+      <nav >
+        <ul className='flex flex-col gap-12 text-dirty-white'>
+          {<li className='hover:cursor-pointer hover:scale-110 hover:text-white duration-150'><span onClick={() => clickHandler("/")} >Home</span></li>}
+          {isLoggedIn && <li className='hover:cursor-pointer hover:scale-110 hover:text-white duration-150'><span onClick={() => clickHandler("/dashboard")} >Dashboard</span></li>}
+          {isLoggedIn && <li className='hover:cursor-pointer hover:scale-110 hover:text-white duration-150'><span onClick={() => clickHandler("/dashboard/favorites")} >My Favorites</span></li>}
+          <li className='hover:cursor-pointer hover:scale-110 hover:text-white duration-150'><span onClick={() => clickHandler("/economy")} >Economic Indicators</span></li>
+          {<li className='hover:cursor-pointer hover:scale-110 hover:text-white duration-150'><span onClick={() => clickHandler("/about")} >About Us</span></li>}
+          {isLoggedIn && <li className='hover:cursor-pointer hover:scale-110 hover:text-white duration-150'><span onClick={() => clickHandler("/account")} >My Account</span></li>}
+          {!isLoggedIn && <li className='hover:cursor-pointer hover:scale-110 hover:text-white duration-150'><span onClick={() => clickHandler("/signin")} >Sign In</span></li>}
+          {isLoggedIn && <li className='hover:cursor-pointer hover:scale-110 hover:text-white duration-150'><span onClick={displaySignOutAlert} >Sign Out</span></li>}
+        </ul>
+      </nav>
     )
   }
 
   return (
-    <div className={styles.container}>
-      {isSignOutOpen && <SignOut closeSignOutAlert={closeSignOutAlert} />}
-      {!isSignOutOpen && renderMenuItems()}
+    <div>
+      {/* {isSignOutOpen && <SignOut closeSignOutAlert={closeSignOutAlert} />} */}
+      {renderMenuItems()}
     </div>
   )
 }
