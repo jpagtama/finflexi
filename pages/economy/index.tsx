0da1297695101954dbcf19d9a8b14c150975d023
Julian Pagtama <jpagtama@gmail.com>
2023-11-08 16:09:33 -0800

    Rework/modernize the economy page

--- pages/economy/index.tsx
+++ pages/economy/index.tsx
@@ -1,12 +1,13 @@
 import React, { useState } from 'react'
 import { prisma } from '@db/index'
 import ChartPicker from '../../components/UI/ChartPicker'
-import Divider from '../../components/UI/Divider'
 import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, PointElement, LineElement, Title, Tooltip, Legend } from 'chart.js'
 import { Line, Bar } from 'react-chartjs-2'
 import type { ChartOptions } from 'chart.js'
 import styles from '../../styles/economy/Economy.module.css'
 import moment from 'moment'
+import EconomicIndicatorSectionGray from './EconomicIndicatorSectionGray'
+import EconomicIndicatorSectionNoBg from './EconomicIndicatorSectionNoBg'
 
 
 interface Props {
@@ -42,8 +43,8 @@ const Economy = ({ gdp, interest_rates, cpi, inflation, unemployment }: Props) =
             datasets: [{
                 label: 'GDP',
                 data: gdp.values.slice(0, gdpTimeFrame).reverse(),
-                borderColor: '#F900BF',
-                backgroundColor: '#F900BF',
+                borderColor: 'none',
+                backgroundColor: '#A5B4FC',
             }]
         }
         const clickHandler = (arg: number) => {
@@ -57,8 +58,7 @@ const Economy = ({ gdp, interest_rates, cpi, inflation, unemployment }: Props) =
         ]
 
         return (
-            <div className={styles.sectionContainer}>
-                <h2 className={styles.sectionTitle}>Gross Domestic Product</h2>
+            <div className='w-full'>
                 <Bar data={data} />
                 <ChartPicker buttons={buttons} />
             </div>
@@ -84,8 +84,8 @@ const Economy = ({ gdp, interest_rates, cpi, inflation, unemployment }: Props) =
             datasets: [{
                 label: 'Percent',
                 data: interest_rates.values.slice(0, interestRatesTimeFrame).reverse(),
-                borderColor: '#F900BF',
-                backgroundColor: '#F900BF',
+                borderColor: '#A5B4FC',
+                backgroundColor: '#A5B4FC',
             }]
         }
         const clickHandler = (arg: number) => {
@@ -97,8 +97,7 @@ const Economy = ({ gdp, interest_rates, cpi, inflation, unemployment }: Props) =
             { title: '10yr', clickHandler: clickHandler, active: interestRatesTimeFrame === 120, duration: 120 }
         ]
         return (
-            <div className={styles.sectionContainer}>
-                <h2 className={styles.sectionTitle}>Federal Interest Rates</h2>
+            <div className='w-full'>
                 <Line options={options} data={data} />
                 <ChartPicker buttons={buttons} />
             </div>
@@ -124,8 +123,8 @@ const Economy = ({ gdp, interest_rates, cpi, inflation, unemployment }: Props) =
             datasets: [{
                 label: 'CPI',
                 data: cpi.values.slice(0, cpiTimeFrame).reverse(),
-                borderColor: '#F900BF',
-                backgroundColor: '#F900BF',
+                borderColor: '#A5B4FC',
+                backgroundColor: '#A5B4FC',
             }]
         }
         const clickHandler = (arg: number) => {
@@ -137,8 +136,7 @@ const Economy = ({ gdp, interest_rates, cpi, inflation, unemployment }: Props) =
             { title: '10yr', clickHandler: clickHandler, active: cpiTimeFrame === 120, duration: 120 }
         ]
         return (
-            <div className={styles.sectionContainer}>
-                <h2 className={styles.sectionTitle}>Consumer Price Index</h2>
+            <div className='w-full'>
                 <Line options={options} data={data} />
                 <ChartPicker buttons={buttons} />
             </div>
@@ -164,8 +162,8 @@ const Economy = ({ gdp, interest_rates, cpi, inflation, unemployment }: Props) =
             datasets: [{
                 label: 'Inflation',
                 data: inflation.values.slice(0, inflationTimeFrame).reverse(),
-                borderColor: '#F900BF',
-                backgroundColor: '#F900BF',
+                borderColor: '#A5B4FC',
+                backgroundColor: '#A5B4FC',
             }]
         }
         const clickHandler = (arg: number) => {
@@ -177,8 +175,7 @@ const Economy = ({ gdp, interest_rates, cpi, inflation, unemployment }: Props) =
             { title: '30yr', clickHandler: clickHandler, active: inflationTimeFrame === 30, duration: 30 }
         ]
         return (
-            <div className={styles.sectionContainer}>
-                <h2 className={styles.sectionTitle}>Inflation</h2>
+            <div className='w-full'>
                 <Line options={options} data={data} />
                 <ChartPicker buttons={buttons} />
             </div>
@@ -204,8 +201,8 @@ const Economy = ({ gdp, interest_rates, cpi, inflation, unemployment }: Props) =
             datasets: [{
                 label: 'Unemployment',
                 data: unemployment.values.slice(0, unemploymentTimeFrame).reverse(),
-                borderColor: '#F900BF',
-                backgroundColor: '#F900BF',
+                borderColor: '#A5B4FC',
+                backgroundColor: '#A5B4FC',
             }]
         }
         const clickHandler = (arg: number) => {
@@ -217,23 +214,36 @@ const Economy = ({ gdp, interest_rates, cpi, inflation, unemployment }: Props) =
             { title: '10yr', clickHandler: clickHandler, active: unemploymentTimeFrame === 120, duration: 120 }
         ]
         return (
-            <div className={styles.sectionContainer}>
-                <h2 className={styles.sectionTitle}>Unemployment</h2>
+            <div className='w-full'>
                 <Line options={options} data={data} />
                 <ChartPicker buttons={buttons} />
             </div>
         )
     }
 
+
+
     return (
-        <div className={styles.container}>
-            <h1 className={styles.title}>Economic Indicators</h1>
-            <Divider />
-            {renderGDP()}
-            {renderInterestRates()}
-            {renderCPI()}
-            {renderInflation()}
-            {renderUnemployment()}
+        <div className='flex flex-col justify-center items-center w-full pt-36'>
+            <h1 className='text-3xl sm:text-5xl md:text-7xl text-center mb-24'><span className='font-bold'>ECONOMIC</span> indicators</h1>
+            <EconomicIndicatorSectionGray title='GROSS DOMESTIC PRODUCT' leadDescBold='Gross Domestic Product' restOfDesc='or GDP, is a measure of the total value of all the goods and services produced in a country during a specific period of time. It is often used as an indicator of the economic health and growth of a country.' >
+                {renderGDP()}
+            </EconomicIndicatorSectionGray>
+            <EconomicIndicatorSectionNoBg title='FEDERAL INTEREST RATES' leadDescBold='Federal interest rates'
+                restOfDesc='are the target range set by the Federal Reserve, the central bank of the United States. The Federal Reserve uses interest rates as a tool to influence the money supply and the economic activity in the country. It affects the borrowing and lending costs of banks, businesses, and consumers, as well as the returns on savings and investments. Higher federal interest rates mean that it is more expensive to borrow money and more rewarding to save money. This can reduce the demand for credit and spending, and increase the supply of savings and investment. Higher federal interest rates can also lower inflation, slow down economic growth, and affect the exchange rate of the U.S. Dollar.'>
+                {renderInterestRates()}
+            </EconomicIndicatorSectionNoBg>
+            <EconomicIndicatorSectionGray title='CONSUMER PRICE INDEX' leadDescBold='Consumer Price Idex' restOfDesc='or CPI, is a measure of the average change over time in the prices paid by urban consumers for a market basket of consumer goods and services. It is one of the most popular measures of inflation and deflation. A higher CPI means that the average prices of goods and services in the economy have increased over a period of time. This can indicate an inflationary situation.' >
+                {renderCPI()}
+            </EconomicIndicatorSectionGray>
+            <EconomicIndicatorSectionNoBg title='INFLATION' leadDescBold='Inflation'
+                restOfDesc='is a general increase of the prices of goods and services in an economy over time. It means that the value of money decreases and the purchasing power of consumers and businesses erodes. It reduces the real income and wealth of people, especially those who have fixed incomes or savings. This means that they can buy fewer goods and services with the same amount of money.'>
+                {renderInflation()}
+            </EconomicIndicatorSectionNoBg>
+            <EconomicIndicatorSectionGray title='UNEMPLOYMENT' leadDescBold='The unemployment indicator' restOfDesc='is a measure of the percentage of the labor force that is actively looking for work but cannot find a job. It affects the productivity and growth of the economy. When people are unemployed, they are not contributing to the output of the economy. This means that the economy is producing less than its potential.' >
+                {renderUnemployment()}
+            </EconomicIndicatorSectionGray>
+
         </div>
     )
 }
