3c1d0553335a17aa9dfdd752c0a4a021e3beae3d
Julian Pagtama <jpagtama@gmail.com>
2023-11-10 11:23:14 -0800

    Modify Footer to display responsively

--- components/layouts/Footer.tsx
+++ components/layouts/Footer.tsx
@@ -5,8 +5,27 @@ import Link from 'next/link'
 const Footer = () => {
   const year = new Date().getFullYear()
   return (
-    <footer className='flex flex-col sm:flex-row justify-center gap-12 bg-gray-600 text-dirty-white min-h-[40vh] w-full py-24 px-4' >
-      <div className='flex flex-col sm:w-1/3 gap-4'>
+    <footer className='flex flex-col sm:flex-row justify-center gap-12 bg-gray-600 text-dirty-white min-h-[40vh] w-full py-24 px-4 overflow-hidden' >
+      <div className='flex gap-8'>
+        <div className='flex flex-col gap-4'>
+          <div className='flex flex-col gap-2'>
+            <span className='text-2xl'>Company</span>
+            <Link className='font-thin hover:text-white duration-150' href='/about'>About Us</Link>
+          </div>
+          <div className='flex flex-col gap-2'>
+            <span className='text-2xl'>Products</span>
+            <Link className='font-thin hover:text-white duration-150' href='/economy'>Economic Indicators</Link>
+            <Link className='font-thin hover:text-white duration-150' href='/dashboard'>Dashboard</Link>
+          </div>
+        </div>
+        <div className='flex flex-col gap-2'>
+          <span className='text-2xl'>Legal</span>
+          <span className='font-thin' >Disclaimer</span>
+          <span className='font-thin' >Terms & Conditions</span>
+          <span className='font-thin' >Data Collection</span>
+        </div>
+      </div>
+      <div className='flex flex-col sm:w-1/3 gap-4 sm:-order-1'>
         <div className='flex flex-col'>
           <span className='text-6xl font-thin'><span className='font-bold'>FIN</span>flexi</span>
           <span className='' >Copyright <span>&copy;</span> {year} Finflexi Ltd.</span>
@@ -15,23 +34,6 @@ const Footer = () => {
           All data provided on Finflexi is provided for informational purposes only, and is not intended for trading or investing purposes. Stock prices displayed in the ticker are from a subset of exchanges, this price does not represent the real-time price.
         </p>
       </div>
-      <div className='flex flex-col gap-2'>
-        <span className='text-2xl'>Company</span>
-        <Link className='font-thin' href='/about'>About Us</Link>
-      </div>
-      <div className='flex flex-col gap-2'>
-        <span className='text-2xl'>Products</span>
-        <Link className='font-thin' href='/economy'>Economic Indicators</Link>
-        <Link className='font-thin' href='/dashboard'>Dashboard</Link>
-      </div>
-      <div className='flex flex-col gap-2'>
-        <span className='text-2xl'>Legal</span>
-        <span className='font-thin' >Disclaimer</span>
-        <span className='font-thin' >Terms & Conditions</span>
-        <span className='font-thin' >Data Collection</span>
-      </div>
-
-
     </footer>
   )
 }
