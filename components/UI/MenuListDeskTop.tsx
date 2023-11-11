6832c6b6d01cf6688e888855db796b7af0f6f39b
Julian Pagtama <jpagtama@gmail.com>
2023-11-10 21:54:28 -0800

    Fix issue where some menu items take long to load

--- components/UI/MenuListDeskTop.tsx
+++ components/UI/MenuListDeskTop.tsx
@@ -1,15 +1,16 @@
 import { menuActions } from '@store/index';
 import { useDispatch, useSelector } from 'react-redux';
+import { authActions, RootState } from '@store/index';
 import { useRouter } from 'next/router'
-import Loading from './Loading';
 
 interface Props {
     isLoggedIn: boolean;
     isLoadingUserInfo: boolean;
 }
 
-const MenuListDeskTop = ({ isLoggedIn, isLoadingUserInfo }: Props) => {
+const MenuListDeskTop = () => {
     const isSignOutOpen = useSelector((state: { "menu": { "signOutOpen": boolean } }) => state.menu.signOutOpen)
+    const { isLoggedIn } = useSelector((state: RootState) => state.auth);
     const router = useRouter();
     const dispatch = useDispatch();
 
@@ -24,21 +25,39 @@ const MenuListDeskTop = ({ isLoggedIn, isLoadingUserInfo }: Props) => {
     const closeSignOutAlert = () => {
         dispatch(menuActions.closeSignOut())
     }
+    const signOut = () => {
+        router.push('/');
+        dispatch(authActions.logout());
+    }
 
 
     return (
-        <nav >
-            {isLoadingUserInfo && <Loading />}
-            {!isLoadingUserInfo && <ul className='flex sm:gap-8 text-dirty-white'>
-                {<li className='hover:cursor-pointer hover:scale-110 hover:text-white duration-150'><span onClick={() => clickHandler("/")} >Home</span></li>}
-                {isLoggedIn && <li className='hover:cursor-pointer hover:scale-110 hover:text-white duration-150'><span onClick={() => clickHandler("/dashboard")} >Dashboard</span></li>}
-                {isLoggedIn && <li className='hover:cursor-pointer hover:scale-110 hover:text-white duration-150'><span onClick={() => clickHandler("/dashboard/favorites")} >My Favorites</span></li>}
+        <nav className='w-screen'>
+            <ul className='flex justify-around text-dirty-white sm:text-xs md:text-base '>
+                <li className='hover:cursor-pointer hover:scale-110 hover:text-white duration-150'><span onClick={() => clickHandler("/")} >Home</span></li>
+                <li className='hover:cursor-pointer hover:scale-110 hover:text-white duration-150' >
+                    <span onClick={() => clickHandler("/dashboard")} >Dashboard</span>
+                </li>
+
+                <li className='hover:cursor-pointer hover:scale-110 hover:text-white duration-150' >
+                    <span onClick={() => clickHandler("/dashboard/favorites")} >My Favorites</span>
+                </li>
                 <li className='hover:cursor-pointer hover:scale-110 hover:text-white duration-150'><span onClick={() => clickHandler("/economy")} >Economic Indicators</span></li>
-                {<li className='hover:cursor-pointer hover:scale-110 hover:text-white duration-150'><span onClick={() => clickHandler("/about")} >About Us</span></li>}
-                {isLoggedIn && <li className='hover:cursor-pointer hover:scale-110 hover:text-white duration-150'><span onClick={() => clickHandler("/account")} >My Account</span></li>}
-                {!isLoggedIn && <li className='hover:cursor-pointer hover:scale-110 hover:text-white duration-150'><span onClick={() => clickHandler("/signin")} >Sign In</span></li>}
-                {isLoggedIn && <li className='hover:cursor-pointer hover:scale-110 hover:text-white duration-150'><span onClick={displaySignOutAlert} >Sign Out</span></li>}
-            </ul>}
+                <li className='hover:cursor-pointer hover:scale-110 hover:text-white duration-150'><span onClick={() => clickHandler("/about")} >About Us</span></li>
+                <li className='hover:cursor-pointer hover:scale-110 hover:text-white duration-150' >
+                    <span onClick={() => clickHandler("/account")} >My Account</span>
+                </li>
+                {!isLoggedIn &&
+                    <li className='hover:cursor-pointer hover:scale-110 hover:text-white duration-150' >
+                        <button onClick={() => clickHandler("/signin")} >Sign In</button>
+                    </li>
+                }
+                {isLoggedIn &&
+                    <li className='hover:cursor-pointer hover:scale-110 hover:text-white duration-150' >
+                        <button onClick={signOut} >Sign Out</button>
+                    </li>
+                }
+            </ul>
         </nav>
     )
 }
