f668620f9556aefac0dbf9fe68134471dbd7e444
Julian Pagtama <jpagtama@gmail.com>
2023-11-30 09:50:32 -0800

    Add formatNumberScale utility

--- utils/utils.tsx
+++ utils/utils.tsx
@@ -11,3 +11,23 @@ export const dbDatetoString = (date: Date) => {
     d = d.toLocaleDateString('en-US')
     return d
 }
+
+export const formatNumberScale = (num: number): string => {
+    // Define the suffixes for different orders of magnitude
+    const suffixes = ["", "K", "M", "B", "T"];
+    // Find the index of the suffix based on the number of digits
+    const index = Math.floor(Math.log10(Math.abs(num)) / 3);
+    // If the index is out of range, return the original number
+    if (index < 0 || index >= suffixes.length) {
+        return num.toString();
+    }
+    // Divide the number by the corresponding power of 1000
+    const shortNum = num / Math.pow(1000, index);
+    // Round the number to two decimal places and add the suffix
+    let result = shortNum.toFixed(2) + suffixes[index];
+    // If the result ends with .00, remove it
+    if (result.endsWith(".00")) {
+        result = result.slice(0, -3);
+    }
+    return result;
+}
