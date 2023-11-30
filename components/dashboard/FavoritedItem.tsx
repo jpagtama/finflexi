ce999c5068d4501eb1cf1ce14dcb6c2277561dda
Julian Pagtama <jpagtama@gmail.com>
2023-11-30 09:55:19 -0800

    Refactor code to use tailwind and remove bloat

--- components/dashboard/FavoritedItem.tsx
+++ components/dashboard/FavoritedItem.tsx
@@ -6,50 +6,27 @@ import { CgMenuGridO } from 'react-icons/cg'
 import styles from '@styles/dashboard/FavoritedItem.module.css'
 
 interface Props {
-    idx: number,
     ticker: string,
     name: string,
     favorited: boolean,
     addToWatchList: (ticker: string, favorited: boolean) => void
-    onDragStart: (e: React.DragEvent<HTMLDivElement>, index: number) => void
-    onDragEnter: (e: React.DragEvent<HTMLDivElement>, index: number) => void
-    onDragEnd: (e: React.DragEvent<HTMLDivElement>, index: number) => void
-    onDragOver: (e: React.DragEvent<HTMLDivElement>) => void
 }
 
-interface ItemType {
-    id: string
-    index: number
-}
-
-const FavoritedItem = ({ idx, ticker, name, favorited, addToWatchList, onDragStart, onDragEnter, onDragEnd, onDragOver }: Props) => {
+const FavoritedItem = ({ ticker, name, favorited, addToWatchList }: Props) => {
 
     return (
-        <div
-            key={ticker}
-            className={`${styles.listItemContainer}`}
-            onDragStart={(e) => onDragStart(e, idx)}
-            onDragEnter={(e) => onDragEnter(e, idx)}
-            onDragEnd={(e) => onDragEnd(e, idx)}
-            onDragOver={(e) => onDragOver(e)}
-            draggable
-        >
-            <div className={styles.grabIconContainer}>
-                <IconContext.Provider value={{ size: '2em', color: 'var(--dark-pink)' }}>
-                    <CgMenuGridO />
-                </IconContext.Provider>
-            </div>
-            <Link href={`/company/${ticker}`} >
-                <a className={styles.linkContainer} >
-                    <li className={`${styles.listItemDetails}`}>
+        <div className='group flex justify-between items-center w-full bg-white' >
+            <div className='flex justify-center items-center'>
+                <Link href={`/company/${ticker}`} >
+                    <div className='flex flex-col w-full p-2 cursor-pointer' >
                         <span className={styles.ticker}>{ticker}</span>
                         <span className={styles.companyName}>{name}</span>
-                    </li>
-                </a>
-            </Link>
-            <span className={styles.starContainer}>
-                <IconContext.Provider value={{ size: '1.5em', color: 'var(--dark-pink)' }}>
-                    <div className={styles.starIcon} onClick={() => addToWatchList(ticker, !favorited)}>{favorited ? <FaStar /> : <FaRegStar />}</div>
+                    </div>
+                </Link>
+            </div>
+            <span className='p-2'>
+                <IconContext.Provider value={{ size: '1.5em' }}>
+                    <button className='cursor-pointer text-indigo-700 hover:scale-105 duration-150' onClick={() => addToWatchList(ticker, !favorited)}>{favorited ? <FaStar /> : <FaRegStar />}</button>
                 </IconContext.Provider>
             </span>
         </div>
